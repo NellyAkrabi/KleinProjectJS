78960e6e89cb5eec59c2c902fa815e0343da123f
Nelli Akrabi <akrabinelly@mail.ru>
2022-09-26 10:37:06 +0200

    NewTAsk remove by Text required

--- index.js
+++ index.js
@@ -2,37 +2,44 @@ const btn = document.getElementById("btn");
 const txt = document.getElementById("input-text");
 
 btn.addEventListener("click", function () {
+
   function required(text) {
     if (text.value.length == 0) {
       alert("Write a task");
+      newTask.remove();
       return false;
     }
+  }
+
+  required(txt);
 
     const boxList = document.getElementById("list");
     const container = document.createElement("div");
+    container.className = "list-container";
 
-    for (let i = 0; i <= 0; i++) {
-      container.className = "list-container";
-      const list = document.createElement("p");
-      list.className = "task";
-      list.innerHTML = txt.value;
 
+     //List
+      const newTask = document.createElement("p");
+      newTask.className = "task";
+      newTask.innerHTML = txt.value;
+      //remove Button
       const deleteBtn = document.createElement("button");
+      //icon
       const icon = document.createElement("i");
       icon.className = "fa-solid fa-trash trash-icon";
       deleteBtn.className = "btn-remove";
+
       deleteBtn.onclick = function () {
-        list.remove();
+        newTask.remove();
         deleteBtn.remove();
         container.remove();
       };
       boxList.appendChild(container);
-      container.appendChild(list);
+      container.appendChild(newTask);
       container.appendChild(deleteBtn);
       deleteBtn.appendChild(icon);
-    }
+
 
     txt.value = "";
-  }
-  required(txt);
+
 });
